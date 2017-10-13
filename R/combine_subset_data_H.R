8765bed4e81f200c82a8817f1c834318cc7192e1
Shreyas Lakhtakia <slakhtakia@partners.org>
2017-10-13 19:26:39 -0400

    Added codefiles to create master data subsets [stage 0]

---
+++ R/combine_subset_data_H.R
@@ -0,0 +1,69 @@
+#----------------------------------------------------------------------------#
+
+#' @title Combine, subset datafiles for a given feature set
+#'
+#' @description For a a given feature set and list of data files (of master data from several cohorts), create a combined master subset containing only observations relevant to the provided cohort empi list.
+#'
+#' @export
+#' @param file_source_list list of paths to .Rds files which are expected to be "master datasets" for multiple patient populations (eg. list of dia files for icmp, bwh_ed_100k cohorts) (list of character)
+#' @param feature_set abbreviation of feature set these files are relevant to (eg. "dia", "prc", "enc") (character)
+#' @param cohort_empi vector of all unique empis in cohort observation set (character vector)
+#' @return
+#' @examples
+
+combine_subset_data <- function(file_source_list, feature_set, cohort_empi){
+
+	# load and combine all datasets relevant to concerned feature_set
+	#-------------------------------------------------------------------------------#
+	
+	list_of_list_feature_set <- c("enc") # for which feature sets are individual cohort files a list of files?
+
+	# accounting for the edge cases such as "enc", outlined in object list_of_list_feature_set, where each dataset in the list consists of a list of datasets
+	if(feature_set %in% list_of_list_feature_set){
+		tryCatch(
+			combined_dt <- readRDS_merge(file_source_list,nested=TRUE), 
+			error=function(e) print(paste0("no source files found / provided for ", feature_set, " feature set"))
+		)
+	} else{
+		tryCatch(
+			combined_dt <- readRDS_merge(file_source_list), 
+			error=function(e) print(paste0("no source files found / provided for ", feature_set, " feature set"))
+		)
+	}
+
+	# subset combined datasets to only include observations from relevant empi / identifiers
+	#-------------------------------------------------------------------------------#
+
+	# accounting for the edge cases such as "enc", outlined in object list_of_list_feature_set, where each dataset in the list consists of a list of datasets
+	if(feature_set %in% list_of_list_feature_set){
+		subset_combined_dt <- mapply(function(dt_list, dt_name){
+			print(sprintf(paste0("Number of rows in combined master set for ", feature_set, "[", dt_name, "]: %d"), nrow(dt_list)))
+			dt_list <- dt_list[empi %in% cohort_empi, ]
+			print(sprintf(paste0("Number of rows in combined master *subset* for ", feature_set, "[", dt_name, "]: %d"), nrow(dt_list)))
+			return(dt_list)
+		}, dt_list = combined_dt, dt_name = names(combined_dt))
+	}else{
+		print(sprintf(paste0("Number of rows in combined master set for ", feature_set, ": %d"), nrow(combined_dt)))
+		subset_combined_dt <- combined_dt[empi %in% cohort_empi, ]
+		print(sprintf(paste0("Number of rows in combined master *subset* for ", feature_set, ": %d"), nrow(subset_combined_dt)))
+	}
+
+	# check
+	#-------------------------------------------------#
+	# warn user if demographics records don't exist for all patients in cohort -- 
+	if(feature_set == "dem"){
+			no_dem <- nrow(cohort[!(empi %in% subset_combined_dt$empi), ])
+			if(no_dem > 0){
+				print(sprintf("WARNING - COMBINED DEMOGRAPHICS FILE MISSING DATA ON %d empis", no_dem))
+			} else{
+				print("All cohort observations are associated with a record in the dem file")
+			}
+	}
+
+	# save the subset files using the feature set as the output file name
+	saveRDS(subset_combined_dt, paste0(source_folder, feature_set, ".Rds"))
+
+	# free up memory
+	rm(combined_dt)
+	rm(subset_combined_dt)
+}
---
+++ R/feature_sourcing_M.R
@@ -0,0 +1,86 @@
+#----------------------------------------------------------------------------#
+
+#' @title Source features (Stage 0).
+#'
+#' @description Create master subsets for each file type from which features are to be assembled. These master subsets are then used in Stage 1 to create features. The idea is to load in all master relevant master cohorts for each file type, subset them to include only records relevant to the cohort at hand, combine these subsetted datasets for each file type, then saving just one subsetted and merged master dataset for each file type, which acts as the source for all feature construction for that feature type. Doing this once allows rapid iteration in stages 1 and 2, since stage 0 is unlikely be repeated more than once for the feature construction process for a given cohort.
+#'
+#' @export
+#' @param cohort_path path to .Rds cohort file which must have the following column names - "empi", "outcome", "t0_date", "outcome_id"; can also contain a "test_set" column; other columns are non-essential but not a problem (character)
+#' @param control_path path to control.R file which contains the user defined parameters for feature construction (character)
+#' @param data_def_path path to data_def_path.R file which contains lists of relevant file paths where master data sets that are to be used in the FC process live (character)
+#' @param feature_path path to feature_selection.csv file which provides options to select variables for FC process (character)
+#' @param feature_set_id suffix indicating an id number or index number for this cohort's feature set (if multiple attempts were made with the same cohort, for example) (character)
+#' @param feature_set_prefix prefix identifying the cohort / feature set that is used in the output file names for all datasets / output created as part of the FC process (character)
+#' @return
+#' @examples
+
+feature_sourcing <- function(cohort_path, control_path, data_def_path, feature_path, 
+	feature_set_id, feature_set_prefix) {
+
+	#----------------------------------------------------------------------------#
+	#                		             SETUP                                   #
+	#----------------------------------------------------------------------------#
+	
+	# source the control / data scripts
+	print(data_def_path)
+	source(data_def_path)
+	
+	print(control_path)
+	source(control_path)
+
+	# initialise
+	#-------------------------------------------------#	
+	feature_initialisation()
+
+	# save a list of the unique empis used to subset the datasets in stage 0 - for downstream checking
+	cohort_empi <- unique(cohort$empi)
+	saveRDS(data.table(empi = cohort_empi), 
+		paste0(modified_folder, "empi_list_", feature_set_prefix, feature_set_id, ".Rds"))
+
+	#----------------------------------------------------------------------------#
+	#                		       FEATURE SOURCING                              #
+	#----------------------------------------------------------------------------#
+	
+	# Initialize
+	error_list   <- c()
+	success_list <- c()
+	print("commence stage 0 - preparing master data subsets by feature set ")
+
+	# Loop over files to create merged and subset versions of each using the combine_subset_data helper function
+	#----------------------------------------------------------------------------#
+	inv_lapply(source_list, function(feature_set) {
+
+		print(paste0("start - ", feature_set))
+
+		sink(paste0(temp_folder, "source_dataset_creation_check_", feature_set,"_",feature_set_name,".txt"), split=TRUE)
+
+		print(sprintf("Number of patients in cohort: %d", length(cohort_empi)))
+		
+		# create a dictionary for which list of source filepaths correspond to which feature set
+		file_source_list_dictionary <- list(
+			dem=dem_file_source, 
+			enc=enc_file_source, 
+			dia=dia_file_source, 
+			prc=prc_file_source, 
+			lvs=lvs_file_source, 
+			lab=lab_file_source, 
+			med=med_file_source, 
+			mic=mic_file_source,
+			ed=ed_file_source, 
+			dia_oncdrs=dia_oncdrs_file_source, 
+			med_oncdrs=med_oncdrs_file_source, 
+			chemo_oncdrs=chemo_oncdrs_file_source, 
+			enc_oncdrs=enc_oncdrs_file_source, 
+			lab_oncdrs=lab_oncdrs_file_source
+		)
+
+		# extract the list of file paths that are relevant to the concerned feature_set
+		file_source_list <- file_source_list_dictionary[[feature_set_name]]
+
+		# call helper to combine, subset and save results for current feature_set
+		combine_subset_data(file_source_list = file_source_list, feature_set = feature_set, cohort_empi = cohort_empi)
+		sink()
+	})
+}	
+
+#----------------------------------------------------------------------------#
